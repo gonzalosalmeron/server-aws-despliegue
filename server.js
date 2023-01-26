4a58e29c958b1eda57cbbd45af311bcf4df863d9
Gonzalo Salmerón <gonzxlosalmeron@gmail.com>
2023-01-26 15:32:32 +0100

    hostname fixed

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ const requestListener = function (req, res) {
 };
 
 const server = http.createServer(requestListener);
-server.listen(port, host, () => {
+server.listen(port, hostname, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
