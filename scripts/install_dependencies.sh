e44b0677fc2b1adbb8f24120435cc2c96fdd1eee
Gonzalo Salmerón <gonzxlosalmeron@gmail.com>
2023-02-16 13:39:25 +0100

    fixed afterinstall

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -11,6 +11,3 @@ nvm install --lts
 
 ln -sf "$NVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/local/bin/node"
 ln -sf "$NVM_DIR/versions/node/$(nvm version)/bin/npm" "/usr/local/bin/npm"
-
-# cd /home/ubuntu/app
-# npm install -y
